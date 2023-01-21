c1e39e880212114a964e871bf439de7243fc1ec1
Tapasvi <tapasviarora2002@gmail.com>
2023-01-22 02:49:01 +0530

    added

---
+++ General.jsx
@@ -0,0 +1,8 @@
+//import React from "react";
+window.__be = window.__be || {};
+window.__be.id = "63cc15caa2284d0007fd1461";
+(function() {
+    var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+    be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+})();
